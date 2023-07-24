f7746bc06fabf481d4785f4a32c365b110343ef2
agusscript <agus.sanchez.240@gmail.com>
2023-07-24 17:11:35 -0300

    Add market-list section styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-function App() {
+import "./App.scss";
+
+function App(): JSX.Element {
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <main>
+      <section className="market-list">
+        <h2 className="title">Supermarket list</h2>
+        <p className="number-items">0 item(s)</p>
+        <button className="add-btn">Add item</button>
+      </section>
+    </main>
   );
 }
 
