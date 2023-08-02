8b37a5e014b5a9493f86a9b74d5f495638ce6581
agusscript <agus.sanchez.240@gmail.com>
2023-08-02 09:33:33 -0300

    Implement localStorage to save items

--- src/SuperMarketList/SuperMarketList.tsx
+++ src/SuperMarketList/SuperMarketList.tsx
@@ -1,5 +1,5 @@
 import "./SuperMarketList.scss";
-import React, { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Item } from "../Item/Item";
 import ItemList from "../Item/ItemList";
 import Footer from "../Footer/Footer";
@@ -7,40 +7,53 @@ import Footer from "../Footer/Footer";
 function SuperMarketList(): JSX.Element {
   const [visibility, setVisibility] = useState(false);
   const [inputValue, setInputValue] = useState(String);
-  const [items, setItems] = useState<Item[]>([]);
   const inputRef = useRef<HTMLInputElement>(null);
 
-  function handleVisibility() {
+  const [items, setItems] = useState<Item[]>(() => {
+    const savedItems = localStorage.getItem("items");
+
+    if (savedItems) {
+      return JSON.parse(savedItems);
+    } else {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  });
+
+  function handleVisibility(): void {
     setVisibility(!visibility);
   }
 
-  function changeVisibility() {
+  function changeVisibility(): string {
     return visibility ? "active" : "hidden";
   }
 
-  function addItems() {
+  function addItems(): void {
     const item = new Item(inputValue, false);
     const inputRefValue = inputRef.current?.value;
 
-    if (inputRefValue == "") {
-      return;
+    if (inputRefValue !== "") {
+      setItems((prevItem) => [...prevItem, item]);
+      clearInput(inputRef.current!);
     }
 
-    setItems((prevItem) => [...prevItem, item]);
-    clearInput(inputRef.current!);
+    event?.preventDefault();
   }
 
-  function clearInput(input: HTMLInputElement) {
+  function clearInput(input: HTMLInputElement): void {
     input.value = "";
   }
 
-  function removeItem(indexToRemove: number) {
+  function removeItem(indexToRemove: number): void {
     const newArray = [...items];
     newArray.splice(indexToRemove, 1);
     setItems(newArray);
   }
 
-  function completeItem(index: number) {
+  function completeItem(index: number): void {
     const newArray = [...items];
     newArray[index].isCompleted = !newArray[index].isCompleted;
     setItems(newArray);
@@ -71,7 +84,7 @@ function SuperMarketList(): JSX.Element {
             Add item
           </button>
 
-          <div className={`input-container ${changeVisibility()}`}>
+          <form className={`input-container ${changeVisibility()}`} onSubmit={addItems}>
             <h2>Add item</h2>
             <input
               ref={inputRef}
@@ -83,19 +96,15 @@ function SuperMarketList(): JSX.Element {
               <button className="close-btn" onClick={handleVisibility}>
                 Close
               </button>
-              <button
-                className="add-btn"
-                onClick={() => {
-                  addItems();
-                  handleVisibility();
-                }}
-              >
+              <button className="add-btn" onClick={handleVisibility}>
                 Add
               </button>
             </div>
-          </div>
+          </form>
+          
         </section>
       </main>
+
       <Footer />
     </>
   );
